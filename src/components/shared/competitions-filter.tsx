99e70bbaebb7c902bdf6ac87f106a4ca75e28f1e
GustavoeDev <gustavo.queiroz@nees.ufal.br>
2025-07-08 14:22:45 -0300

    feat: create filter component to filter by competitions

---
+++ src/components/shared/competitions-filter.tsx
@@ -0,0 +1,86 @@
+"use client"
+
+import * as React from "react"
+import { CheckIcon, ChevronsUpDownIcon } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+    Command,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandList,
+} from "@/components/ui/command"
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
+
+const frameworks = [
+    {
+        value: "handebol",
+        label: "Handebol Misto",
+    },
+    {
+        value: "basquetemasculino",
+        label: "Basquete Masculino",
+    },
+    {
+        value: "futsalfeminino",
+        label: "Futsal Feminino",
+    }
+]
+
+export default function CompetitionsFilter() {
+    const [open, setOpen] = React.useState(false)
+    const [value, setValue] = React.useState("")
+
+    return (
+        <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+                <Button
+                    variant="outline"
+                    role="combobox"
+                    aria-expanded={open}
+                    className="w-[250px] justify-between rounded-none rounded-tl-lg rounded-bl-lg"
+                >
+                    {value
+                        ? frameworks.find((framework) => framework.value === value)?.label
+                        : "Selecione a competição..."}
+                    <ChevronsUpDownIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-[200px] p-0">
+                <Command>
+                    <CommandInput placeholder="Procurar competição..." />
+                    <CommandList>
+                        <CommandEmpty>No framework found.</CommandEmpty>
+                        <CommandGroup>
+                            {frameworks.map((framework) => (
+                                <CommandItem
+                                    key={framework.value}
+                                    value={framework.value}
+                                    onSelect={(currentValue) => {
+                                        setValue(currentValue === value ? "" : currentValue)
+                                        setOpen(false)
+                                    }}
+                                >
+                                    <CheckIcon
+                                        className={cn(
+                                            "mr-2 h-4 w-4",
+                                            value === framework.value ? "opacity-100" : "opacity-0"
+                                        )}
+                                    />
+                                    {framework.label}
+                                </CommandItem>
+                            ))}
+                        </CommandGroup>
+                    </CommandList>
+                </Command>
+            </PopoverContent>
+        </Popover>
+    )
+}
