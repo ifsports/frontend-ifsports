b631486611321a579a6ff6872ce632e5064e7aef
GustavoeDev <gustavo.queiroz@nees.ufal.br>
2025-07-08 14:23:26 -0300

    feat: create a page with all the games to watch live

---
+++ src/app/jogos/page.tsx
@@ -0,0 +1,22 @@
+import GamesContainer from "@/components/features/games/games-container";
+
+export default function GamesPage(){
+    return (
+        <section>
+            <div className="my-12 flex flex-col gap-3 max-w-[39.375rem]">
+                <h2 className="font-title text-3xl font-bold">Acompanhar jogos</h2>
+                <p className="text-[#4F4F4F]">Nesta seção, você poderá acompanhar as partidas em tempo real,
+                    visualizando tudo o que está acontecendo em cada jogo, minuto a minuto.
+                    Além disso, é possível interagir com outros usuários por meio de um chat
+                    ao vivo, tornando a experiência ainda mais dinâmica e imersiva. Aqui,
+                    você não perde nenhum lance e ainda compartilha suas impressões com outros
+                    torcedores.</p>
+            </div>
+
+            <div>
+                <GamesContainer />
+            </div>
+
+        </section>
+    )
+}
---
+++ src/components/features/games/games-container.tsx
@@ -0,0 +1,65 @@
+import CompetitionsFilter from "@/components/shared/competitions-filter";
+import {Button} from "@/components/ui/button";
+import {Search} from "lucide-react";
+import GameLive from "@/components/shared/game-live";
+
+import {
+    Pagination,
+    PaginationContent,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious,
+} from "@/components/ui/pagination"
+
+export default function GamesContainer() {
+    return (
+        <>
+            <div className="flex items-start justify-between mb-8">
+                <h3 className="font-title text-2xl font-bold">Todos os jogos</h3>
+                <div className="flex">
+                    <CompetitionsFilter />
+                    <Button variant={"link"} className="rounded-none rounded-tr-lg rounded-br-lg border-none cursor-pointer text-[#ffffff] bg-[#4CAF50] hover:bg-[#147A02]">
+                        <Search size={18} />
+                    </Button>
+                </div>
+            </div>
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                <GameLive variant="live" />
+                <GameLive />
+                <GameLive />
+                <GameLive />
+                <GameLive variant="live" />
+                <GameLive />
+            </div>
+
+            <Pagination className="my-16">
+                <PaginationContent>
+                    <PaginationItem>
+                        <PaginationPrevious href="#" />
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationLink href="#">1</PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationLink href="#">2</PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationEllipsis />
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationLink href="#">9</PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationLink href="#">10</PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationNext href="#" />
+                    </PaginationItem>
+                </PaginationContent>
+            </Pagination>
+        </>
+    )
+}
