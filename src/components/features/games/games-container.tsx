44c50623689d3a8b6e20fc767d5c5a7cee8101fc
GustavoeDev <gustavoe.dev@gmail.com>
2025-07-23 08:13:32 -0300

    feat: create home page with sports competitions carousel and responsive layout

--- src/components/features/games/games-container.tsx
+++ src/components/features/games/games-container.tsx
@@ -14,12 +14,28 @@ import {
 } from "@/components/ui/pagination"
 
 export default function GamesContainer() {
+
+    const competitions = [
+        {
+            value: "handebol",
+            label: "Handebol Misto",
+        },
+        {
+            value: "basquetemasculino",
+            label: "Basquete Masculino",
+        },
+        {
+            value: "futsalfeminino",
+            label: "Futsal Feminino",
+        }
+    ]
+
     return (
         <>
             <div className="flex items-start justify-between mb-8 max-[640px]:flex-col max-[640px]:gap-3">
                 <h3 className="font-title text-2xl font-bold">Todos os jogos</h3>
                 <div className="flex">
-                    <CompetitionsFilter />
+                    <CompetitionsFilter label="a competição" data={competitions} />
                     <Button variant={"link"} className="rounded-none rounded-tr-lg rounded-br-lg border-none cursor-pointer text-[#ffffff] bg-[#4CAF50] hover:bg-[#147A02]">
                         <Search size={18} />
                     </Button>
--- src/components/shared/competitions-filter.tsx
+++ src/components/shared/competitions-filter.tsx
@@ -19,22 +19,15 @@ import {
     PopoverTrigger,
 } from "@/components/ui/popover"
 
-const frameworks = [
-    {
-        value: "handebol",
-        label: "Handebol Misto",
-    },
-    {
-        value: "basquetemasculino",
-        label: "Basquete Masculino",
-    },
-    {
-        value: "futsalfeminino",
-        label: "Futsal Feminino",
-    }
-]
+interface CompetitionsFilterProps {
+    label: string;
+    data: {
+        value: string;
+        label: string;
+    }[]
+}
 
-export default function CompetitionsFilter() {
+export default function CompetitionsFilter({ label, data } : CompetitionsFilterProps) {
     const [open, setOpen] = React.useState(false)
     const [value, setValue] = React.useState("")
 
@@ -48,21 +41,21 @@ export default function CompetitionsFilter() {
                     className="max-w-[250px] justify-between rounded-none rounded-tl-lg rounded-bl-lg"
                 >
                     {value
-                        ? frameworks.find((framework) => framework.value === value)?.label
-                        : "Selecione a competição..."}
+                        ? data.find((dataValue) => dataValue.value === value)?.label
+                        : `Selecione ${label}...`}
                     <ChevronsUpDownIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
                 </Button>
             </PopoverTrigger>
             <PopoverContent className="w-[200px] p-0">
                 <Command>
-                    <CommandInput placeholder="Procurar competição..." />
+                    <CommandInput placeholder={`Procurar ${label}...`} />
                     <CommandList>
-                        <CommandEmpty>No framework found.</CommandEmpty>
+                        <CommandEmpty>No {label.split(" ")[1]} found.</CommandEmpty>
                         <CommandGroup>
-                            {frameworks.map((framework) => (
+                            {data.map((dataValue) => (
                                 <CommandItem
-                                    key={framework.value}
-                                    value={framework.value}
+                                    key={dataValue.value}
+                                    value={dataValue.value}
                                     onSelect={(currentValue) => {
                                         setValue(currentValue === value ? "" : currentValue)
                                         setOpen(false)
@@ -71,10 +64,10 @@ export default function CompetitionsFilter() {
                                     <CheckIcon
                                         className={cn(
                                             "mr-2 h-4 w-4",
-                                            value === framework.value ? "opacity-100" : "opacity-0"
+                                            value === dataValue.value ? "opacity-100" : "opacity-0"
                                         )}
                                     />
-                                    {framework.label}
+                                    {dataValue.label}
                                 </CommandItem>
                             ))}
                         </CommandGroup>
